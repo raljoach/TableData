38115b1e280f35fb8a4075b6bfa3478ef12069fa
Ralph Joachim <raljoach@gmail.com>
2021-03-06 20:52:25 -0800

    initial code

---
+++ Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableData
+{
+    /* Table is a list of rows
+     * Each row contains fields
+     */
+    public class Table
+    {
+        private List<TableRow> _rows = new List<TableRow>();
+
+        public void Add(TableRow row)
+        {
+            _rows.Add(row);
+        }
+
+        public void ExportCsv(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TableRow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableData
+{
+    /* Table Row contains fields
+     * Each field has a name and value
+     */
+    public class TableRow
+    {
+        private Dictionary<string, string> _fields = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        public void Set(string column, string value)
+        {
+            column = column.Trim();
+            if (_fields.ContainsKey(column))
+            {
+                _fields[column] = value;
+            }
+            else
+            {
+                _fields.Add(column, value);
+            }
+        }
+    }
+}
